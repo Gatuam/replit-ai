8c962e69ca78298362d4c72979cd45a73e05bc72
EK GAUTHAM <sggtm42@gmail.com>
2025-08-17 18:38:48 +0700

    setup ai agentkit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,30 @@
-'use client'
+"use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
-export default  function Home() {
+export default function Home() {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({
-    onSuccess : ()=> {
-      toast.success("Backgrond job start")
-    }
-  }))
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Backgrond job start");
+      },
+    })
+  );
   return (
-    <div className="p-4 max-w-7xl ">
-      <Button
-      onClick={()=> invoke.mutate({ text : 'wtf' })}
-      >
+    <div className="p-4 max-w-3xl mx-auto flex gap-7 ">
+      <Input
+        className=" border border-[#111]"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Button onClick={() => invoke.mutate({ value: value })}>
         Invoke background job
       </Button>
     </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,32 @@
 import { inngest } from "./client";
+import { anthropic, createAgent } from "@inngest/agent-kit";
+
+const AI_KEY = process.env.ANTHROPIC_API_KEY || "";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    const writer = createAgent({
+      name: "writer",
+      system:
+        "You are an expert writer.  You write readable, concise, simple content.write in 10 words",
+      model: anthropic({
+        model: "claude-3-haiku-20240307",
+        apiKey: AI_KEY,
+        defaultParameters: {
+          max_tokens: 100,
+          temperature: 0.3,
+        },
+      }),
+    });
+    const { output } = await writer.run(
+      `Write the following text : ${event?.data?.value}`
+    );
+    const text = (output[0] as any)?.content ?? "";
+
+    console.log("AI output:", text);
+
+    return { message: `outPut by ai ::::: ${text}` };
+  }
 );
