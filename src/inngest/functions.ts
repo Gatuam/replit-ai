bbf1a4595d321369ba80643beb39e5dd2478537e
EK GAUTHAM <sggtm42@gmail.com>
2025-08-31 16:29:09 +0700

    model test

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -73,7 +73,7 @@ export const codeAgentFunction = inngest.createFunction(
       description: "An expert coding agent",
       system: PROMPT,
       model: anthropic({
-        model: "claude-3-5-haiku-20241022",
+        model: "claude-3-5-sonnet-20241022",
         apiKey: AI_KEY,
         defaultParameters: {
           max_tokens: 4096,
